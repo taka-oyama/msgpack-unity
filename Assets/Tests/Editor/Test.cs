9f0cf1f082f8b71edd96e95459e8cf4a36c9b48d
Taka Oyama <taka.oyama@gmail.com>
2018-02-23 00:13:34 +0900

    start adding tests

---
+++ Assets/Tests/Editor/Test.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UniMsgPack;
+using NUnit.Framework;
+using System.IO;
+
+public class Test
+{
+	static string basePath;
+
+	static Test()
+	{
+		basePath = Application.dataPath + "/Tests/Files";
+	}
+
+	public static byte[] ReadFile(string path)
+	{
+		return File.ReadAllBytes(path);
+	}
+
+	[Test]
+	public void UnpackFixArrayMin()
+	{
+		byte[] bytes = ReadFile(basePath + "/Arrays/FixArrayMin.mpack");
+		int[] test = MsgPack.Unpack<int[]>(bytes);
+
+		Assert.AreEqual(test.Length, 0);
+	}
+
+	[Test]
+	public void UnpackFixArrayMax()
+	{
+		byte[] bytes = ReadFile(basePath + "/Arrays/FixArrayMax.mpack");
+		int[] test = MsgPack.Unpack<int[]>(bytes);
+
+		Assert.AreEqual(test.Length, 15);
+	}
+}
