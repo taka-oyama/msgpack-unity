2b868837ed1bc5c44db81c490659e4243e8d5223
Taka Oyama <taka.oyama@gmail.com>
2018-02-25 00:33:59 +0900

    add boolean tests

---
+++ Assets/Tests/Editor/BoolTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using UniMsgPack;
+using UnityEngine;
+
+public class BoolTest : TestBase
+{
+	[Test]
+	public void UnpackTrue()
+	{
+		byte[] bytes = ReadFile(basePath + "/Bools/True.mpack");
+		bool boolValue = MsgPack.Unpack<bool>(bytes);
+
+		Assert.IsTrue(boolValue);
+	}
+
+	[Test]
+	public void UnpackFalse()
+	{
+		byte[] bytes = ReadFile(basePath + "/Bools/False.mpack");
+		bool boolValue = MsgPack.Unpack<bool>(bytes);
+
+		Assert.IsFalse(boolValue);
+	}
+}
