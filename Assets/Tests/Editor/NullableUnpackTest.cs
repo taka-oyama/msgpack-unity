9610be9baab5eeb4a7dddab6400d24aacddc513a
Taka Oyama <taka.oyama@gmail.com>
2018-02-26 01:12:09 +0900

    add nullable test

---
+++ Assets/Tests/Editor/NullableUnpackTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace UniMsgPack.Tests
+{
+	public class NullableUnpackTest : TestBase
+	{
+		enum MyEnum
+		{
+			Foo = 1,
+		}
+
+		[Test]
+		public void UnpackNullableInt()
+		{
+			byte[] bytes = ReadFile(basePath + "/Nullables/Int.mpack");
+			int? value = MsgPack.Unpack<int?>(bytes);
+			Assert.AreEqual(1, value);
+		}
+
+		[Test]
+		public void UnpackNullableIntAsNull()
+		{
+			byte[] bytes = ReadFile(basePath + "/Nullables/Nil.mpack");
+			int? value = MsgPack.Unpack<int?>(bytes);
+			Assert.AreEqual(null, value);
+		}
+
+		[Test]
+		public void UnpackNullableEnum()
+		{
+			byte[] bytes = ReadFile(basePath + "/Nullables/EnumInt.mpack");
+			MyEnum value = MsgPack.Unpack<MyEnum>(bytes);
+			Assert.AreEqual(MyEnum.Foo, value);
+		}
+
+
+		[Test]
+		public void UnpackNullableEnumAsNull()
+		{
+			byte[] bytes = ReadFile(basePath + "/Nullables/Nil.mpack");
+			MyEnum? value = MsgPack.Unpack<MyEnum?>(bytes);
+			Assert.AreEqual(null, value);
+		}
+	}
+}
