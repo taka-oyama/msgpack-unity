e9f9da0c8e11f09967b2e737685de1b07d2434fd
Taka Oyama <taka.oyama@gmail.com>
2018-03-03 14:15:04 +0900

    refactor

--- Assets/MsgPackReader.cs
+++ Assets/MsgPackReader.cs
@@ -34,45 +34,45 @@ namespace UniMsgPack
 		object Read(Type type, Format format)
 		{
 			if(type.IsPrimitive) {
-				if(type == typeof(int)) return ReadInt32(format);
-				if(type == typeof(bool)) return ReadBoolean(format);
+				if(type == typeof(int)) return ReadInt(format);
+				if(type == typeof(bool)) return ReadBool(format);
 				if(type == typeof(float)) return ReadFloat(format);
 				if(type == typeof(double)) return ReadDouble(format);
-				if(type == typeof(uint)) return ReadUInt32(format);
-				if(type == typeof(sbyte)) return ReadInt8(format);
-				if(type == typeof(byte)) return ReadUInt8(format);
-				if(type == typeof(short)) return ReadInt16(format);
-				if(type == typeof(ushort)) return ReadUInt16(format);
-				if(type == typeof(long)) return ReadInt64(format);
-				if(type == typeof(ulong)) return ReadUInt64(format);
+				if(type == typeof(uint)) return ReadUInt(format);
+				if(type == typeof(sbyte)) return ReadSbyte(format);
+				if(type == typeof(byte)) return ReadByte(format);
+				if(type == typeof(short)) return ReadShort(format);
+				if(type == typeof(ushort)) return ReadUShort(format);
+				if(type == typeof(long)) return ReadLong(format);
+				if(type == typeof(ulong)) return ReadULong(format);
 				throw new NotSupportedException(type + " is not a supported primitive type");
 			}
 			if(type == typeof(string)) return ReadString(format);
 			if(type == typeof(byte[])) return ReadBinary(format);
-			if(type == typeof(DateTime)) return ReadTimestamp(format);
+			if(type == typeof(DateTime)) return ReadDateTime(format);
 			if(type.IsEnum) return ReadEnum(type, format);
 			if(IsNullable(type)) return ReadNullable(type, format);
 			if(type.IsArray) return ReadArray(type, format);
 			if(typeof(IList).IsAssignableFrom(type)) return ReadList(type, format);
 			if(typeof(IDictionary).IsAssignableFrom(type)) return ReadDictionary(type, format);
-			return ReadMap(type, format);
+			return ReadClassOrStruct(type, format);
 		}
 
-		bool ReadBoolean(Format format)
+		bool ReadBool(Format format)
 		{
 			if(format == Format.False) return false;
 			if(format == Format.True) return true;
 			throw new FormatException();
 		}
 
-		byte ReadUInt8(Format format)
+		byte ReadByte(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return ExtractUInt8();
 			throw new FormatException();
 		}
 
-		ushort ReadUInt16(Format format)
+		ushort ReadUShort(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return ExtractUInt8();
@@ -80,7 +80,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		uint ReadUInt32(Format format)
+		uint ReadUInt(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return ExtractUInt8();
@@ -89,7 +89,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		ulong ReadUInt64(Format format)
+		ulong ReadULong(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return ExtractUInt8();
@@ -99,7 +99,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		sbyte ReadInt8(Format format)
+		sbyte ReadSbyte(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return (sbyte)ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return Convert.ToSByte(ExtractUInt8());
@@ -108,7 +108,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		short ReadInt16(Format format)
+		short ReadShort(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return ExtractUInt8();
@@ -119,7 +119,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		int ReadInt32(Format format)
+		int ReadInt(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return ExtractUInt8();
@@ -132,7 +132,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		long ReadInt64(Format format)
+		long ReadLong(Format format)
 		{
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) return ExtractPositiveFixInt(format);
 			if(format == Format.UInt8) return ExtractUInt8();
@@ -149,9 +149,9 @@ namespace UniMsgPack
 
 		float ReadFloat(Format format)
 		{
-			if(format == Format.Float32) return ExtractFloat();
+			if(format == Format.Float32) return ExtractFloat32();
 			if(format == Format.Float64) {
-				double value = ExtractDouble();
+				double value = ExtractFloat64();
 				if(value > float.MaxValue) {
 					throw new InvalidCastException(string.Format("{0} is too big for a float", value));
 				}
@@ -165,8 +165,8 @@ namespace UniMsgPack
 
 		double ReadDouble(Format format)
 		{
-			if(format == Format.Float32) return ExtractFloat();
-			if(format == Format.Float64) return ExtractDouble();
+			if(format == Format.Float32) return ExtractFloat32();
+			if(format == Format.Float64) return ExtractFloat64();
 			throw new FormatException();
 		}
 
@@ -205,7 +205,7 @@ namespace UniMsgPack
 		object ReadEnum(Type type, Format format)
 		{
 			if(format.IsInt()) {
-				int value = ReadInt32(format);
+				int value = ReadInt(format);
 				if(Enum.IsDefined(type, value)) {
 					return Enum.ToObject(type, value);
 				}
@@ -223,7 +223,7 @@ namespace UniMsgPack
 			if(format == Format.Nil) {
 				return Array.CreateInstance(elementType, 0);
 			}
-            int size = ExtractArraySize(format);
+			int size = ExtractArraySize(format);
 			if(size >= 0) {
 				Array array = Array.CreateInstance(elementType, size);
 				for(int i = 0; i < size; i++) {
@@ -242,7 +242,7 @@ namespace UniMsgPack
 			if(format == Format.Nil) {
 				return list;
 			}
-            int size = ExtractArraySize(format);
+			int size = ExtractArraySize(format);
 			if(size >= 0) {
 				for(int i = 0; i < size; i++) {
 					list.Add(Read(elementType));
@@ -252,30 +252,30 @@ namespace UniMsgPack
 			throw new FormatException(string.Format("Invalid Format {0} for {1}", format, type));
 		}
 
-        object ReadDictionary(Type type, Format format)
-        {
-            if (format == Format.Nil) {
-                return null;
-            }
-            int size = ExtractMapSize(format);
-            if (size == -1) {
-                throw new FormatException(string.Format("Invalid Map size {0} for {1}", size, type));
-            }
-            IDictionary dictionary = (IDictionary)Activator.CreateInstance(type);
-            Type[] types = type.GetGenericArguments();
-            while (size > 0) {
-                dictionary.Add(Read(types[0]), Read(types[1]));
-                size = size - 1;
-            }
-            return dictionary;
-        }
-
-		object ReadMap(Type type, Format format)
+		object ReadDictionary(Type type, Format format)
 		{
 			if(format == Format.Nil) {
 				return null;
 			}
-            int size = ExtractMapSize(format);
+			int size = ExtractMapSize(format);
+			if(size == -1) {
+				throw new FormatException(string.Format("Invalid Map size {0} for {1}", size, type));
+			}
+			IDictionary dictionary = (IDictionary)Activator.CreateInstance(type);
+			Type[] types = type.GetGenericArguments();
+			while(size > 0) {
+				dictionary.Add(Read(types[0]), Read(types[1]));
+				size = size - 1;
+			}
+			return dictionary;
+		}
+
+		object ReadClassOrStruct(Type type, Format format)
+		{
+			if(format == Format.Nil) {
+				return null;
+			}
+			int size = ExtractMapSize(format);
 			if(size == -1) {
 				throw new FormatException(string.Format("Invalid Map size {0} for {1}", size, type));
 			}
@@ -285,7 +285,8 @@ namespace UniMsgPack
 				FieldInfo field = MapResolver.GetField(type, name);
 				if(field != null) {
 					field.SetValue(obj, Read(field.FieldType));
-				} else {
+				}
+				else {
 					Skip();
 				}
 				size = size - 1;
@@ -293,32 +294,24 @@ namespace UniMsgPack
 			return obj;
 		}
 
-		DateTime ReadTimestamp(Format format)
+		DateTime ReadDateTime(Format format)
 		{
 			if(format == Format.FixExt4) {
-				if(stream.ReadByte() != -1) {
-					throw new FormatException("Not a Timestamp extension format!");
+				if(stream.ReadByte() == -1) {
+					return ExtractTimestamp32();
 				}
-				return epochTime.AddSeconds(ExtractUInt32());
 			}
 			if(format == Format.FixExt8) {
-				if(stream.ReadByte() != -1) {
-					throw new FormatException("Not a Timestamp extension format!");
+				if(stream.ReadByte() == -1) {
+					return ExtractTimestamp64();
 				}
-				stream.Read(staticBuffer, 0, 8);
-				uint nanoseconds = ((uint)staticBuffer[0] << 22) | ((uint)staticBuffer[1] << 14) | ((uint)staticBuffer[2] << 6) | (uint)staticBuffer[3] >> 2;
-				ulong seconds = ((ulong)(staticBuffer[3] & 0x3) << 32) | ((ulong)staticBuffer[4] << 24) | ((ulong)staticBuffer[5] << 16) | ((ulong)staticBuffer[6] << 8) | (ulong)staticBuffer[7];
-				return epochTime.AddTicks(nanoseconds / 100).AddSeconds(seconds);
 			}
 			if(format == Format.Ext8) {
-				if(stream.ReadByte() != 12) {
-					throw new FormatException("Incorrect Timestamp96 extension length!");
+				if(stream.ReadByte() == 12) {
+					if(stream.ReadByte() == -1) {
+						return ExtractTimestamp96();
+					}
 				}
-				if(stream.ReadByte() != -1) {
-					throw new FormatException("Not a Timestamp extension format!");
-				}
-				DateTime time = epochTime.AddTicks(ExtractUInt32() / 100).AddSeconds(ExtractInt64());
-				return time;
 			}
 			throw new FormatException();
 		}
@@ -331,7 +324,7 @@ namespace UniMsgPack
 			if(format == Format.True) { return; }
 			if(format.Between(Format.PositiveFixIntMin, Format.PositiveFixIntMax)) { return; }
 			if(format.Between(Format.NegativeFixIntMin, Format.NegativeFixIntMax)) { return; }
-			if(format == Format.UInt8  || format == Format.Int8 ) { FastForward(1); return; }
+			if(format == Format.UInt8 || format == Format.Int8) { FastForward(1); return; }
 			if(format == Format.UInt16 || format == Format.Int16) { FastForward(2); return; }
 			if(format == Format.UInt32 || format == Format.Int32) { FastForward(4); return; }
 			if(format == Format.UInt64 || format == Format.Int64) { FastForward(8); return; }
@@ -456,7 +449,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		float ExtractFloat()
+		float ExtractFloat32()
 		{
 			if(stream.Read(staticBuffer, 0, 4) == 4) {
 				if(BitConverter.IsLittleEndian) {
@@ -467,7 +460,7 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-		double ExtractDouble()
+		double ExtractFloat64()
 		{
 			if(stream.Read(staticBuffer, 0, 8) == 8) {
 				if(BitConverter.IsLittleEndian) {
@@ -476,6 +469,24 @@ namespace UniMsgPack
 				return BitConverter.ToDouble(staticBuffer, 0);
 			}
 			throw new FormatException();
+		}
+
+		DateTime ExtractTimestamp32()
+		{
+			return epochTime.AddSeconds(ExtractUInt32());
+		}
+
+		DateTime ExtractTimestamp64()
+		{
+			stream.Read(staticBuffer, 0, 8);
+			uint nanoseconds = ((uint)staticBuffer[0] << 22) | ((uint)staticBuffer[1] << 14) | ((uint)staticBuffer[2] << 6) | (uint)staticBuffer[3] >> 2;
+			ulong seconds = ((ulong)(staticBuffer[3] & 0x3) << 32) | ((ulong)staticBuffer[4] << 24) | ((ulong)staticBuffer[5] << 16) | ((ulong)staticBuffer[6] << 8) | (ulong)staticBuffer[7];
+			return epochTime.AddTicks(nanoseconds / 100).AddSeconds(seconds);
+		}
+
+		DateTime ExtractTimestamp96()
+		{
+			return epochTime.AddTicks(ExtractUInt32() / 100).AddSeconds(ExtractInt64());
 		}
 
 		int ExtractStringSize(Format format)
@@ -496,13 +507,13 @@ namespace UniMsgPack
 			throw new FormatException();
 		}
 
-        int ExtractArraySize(Format format)
-        {
+		int ExtractArraySize(Format format)
+		{
 			if(format.Between(Format.FixArrayMin, Format.FixArrayMax)) return ((int)format & 0xf);
 			if(format == Format.Array16) return ExtractUInt16();
 			if(format == Format.Array32) return Convert.ToInt32(ExtractUInt32());
-            return -1;
-        }
+			return -1;
+		}
 
 		int ExtractMapSize(Format format)
 		{
