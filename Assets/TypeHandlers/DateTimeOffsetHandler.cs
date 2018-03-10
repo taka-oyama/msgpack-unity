240bd99b4031bed2354af8790d8493f5177bfc8e
Taka Oyama <taka.oyama@gmail.com>
2018-03-11 00:16:18 +0900

    had an incorrect handler

--- Assets/TypeHandlers/DateTimeOffsetHandler.cs
+++ Assets/TypeHandlers/DateTimeOffsetHandler.cs
@@ -5,10 +5,17 @@ namespace UniMsgPack
 {
 	public class DateTimeOffsetHandler : ITypeHandler
 	{
+		DateTimeHandler baseHandler;
+
 		public object Read(Format format, FormatReader reader)
 		{
-			ITypeHandler handler = TypeHandlers.Get(typeof(DateTimeOffset));
-			return new DateTimeOffset((DateTime)handler.Read(format, reader));
+			return new DateTimeOffset(ReadDateTime(format, reader));
+		}
+
+		DateTime ReadDateTime(Format format, FormatReader reader)
+		{
+			this.baseHandler = baseHandler ?? TypeHandlers.Get(typeof(DateTime)) as DateTimeHandler;
+			return (DateTime)baseHandler.Read(format, reader);
 		}
 	}
 }
