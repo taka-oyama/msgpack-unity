dffd28756e12e89514b235f64ac4f6eefdce76f4
Taka Oyama <taka.oyama@gmail.com>
2018-03-18 22:15:30 +0900

    map pack test

---
+++ Assets/Tests/Editor/MapPackTest.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace UniMsgPack.Tests
+{
+	public class MapPackTest : TestBase
+	{
+		struct StructMap
+		{
+			public int a;
+			public int b;
+			public int c;
+			public int? d;
+		}
+
+		class ClassMap
+		{
+			public int a = 0;
+			public int b = 0;
+			public int c = 0;
+			public int? d = null;
+		}
+
+		class PrivateClassMap
+		{
+			private int a = 0;
+			public int A { get { return a; } }
+		}
+
+		class MixedMap
+		{
+			public ClassMap inner = null;
+		}
+
+		[Test]
+		public void Struct()
+		{
+			StructMap map = MsgPack.Unpack<StructMap>(ReadFile("Maps/Struct"));
+			Assert.AreEqual(map.a, 1);
+			Assert.AreEqual(map.b, 2);
+			Assert.AreEqual(map.c, 0);
+			Assert.AreEqual(map.d, null);
+		}
+
+		[Test]
+		public void Class()
+		{
+			ClassMap map = MsgPack.Unpack<ClassMap>(ReadFile("Maps/Struct"));
+			Assert.AreEqual(map.a, 1);
+			Assert.AreEqual(map.b, 2);
+			Assert.AreEqual(map.c, 0);
+			Assert.AreEqual(map.d, null);
+		}
+
+		[Test]
+		public void ClassWithPrivateFields()
+		{
+			PrivateClassMap map = MsgPack.Unpack<PrivateClassMap>(ReadFile("Maps/Struct"));
+			Assert.AreEqual(map.A, 1);
+		}
+
+		[Test]
+		public void MapSkippable()
+		{
+			MixedMap map = MsgPack.Unpack<MixedMap>(ReadFile("Maps/MapSkippable"));
+			Assert.AreEqual(map.inner.a, 1);
+			Assert.AreEqual(map.inner.b, 2);
+		}
+
+		[Test]
+		public void FixMapMin()
+		{
+			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/FixMapMin"));
+			Assert.AreEqual(0, dict.Count);
+		}
+
+		[Test]
+		public void FixMapMax()
+		{
+			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/FixMapMax"));
+			Assert.AreEqual(15, dict.Count);
+			for(int i = 0; i < 15; i++) {
+				Assert.AreEqual(1, dict[i]);
+			}
+		}
+
+		[Test]
+		public void Map16Min()
+		{
+			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/Map16Min"));
+			Assert.AreEqual(16, dict.Count);
+			for(int i = 0; i < 16; i++) {
+				Assert.AreEqual(1, dict[i]);
+			}
+		}
+
+		[Test]
+		public void Map16Max()
+		{
+			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/Map16Max"));
+			Assert.AreEqual(ushort.MaxValue, dict.Count);
+			for(int i = 0; i < ushort.MaxValue; i++) {
+				Assert.AreEqual(1, dict[i]);
+			}
+		}
+
+		[Test]
+		public void Map32Min()
+		{
+			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/Map32Min"));
+			Assert.AreEqual(ushort.MaxValue + 1, dict.Count);
+			for(int i = 0; i < ushort.MaxValue + 1; i++) {
+				Assert.AreEqual(1, dict[i]);
+			}
+		}
+	}
+}
--- Assets/Tests/Editor/MapUnpackTest.cs
+++ Assets/Tests/Editor/MapUnpackTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -9,8 +10,6 @@ namespace UniMsgPack.Tests
 		struct StructMap
 		{
 			public int a;
-			public int b;
-			public int c;
 			public int? d;
 		}
 
@@ -24,63 +23,64 @@ namespace UniMsgPack.Tests
 
 		class PrivateClassMap
 		{
-			private int a = 0;
+			private int a = 1;
 			public int A { get { return a; } }
-		}
-
-		class MixedMap
-		{
-			public ClassMap inner = null;
 		}
 
 		[Test]
 		public void Struct()
 		{
-			StructMap map = MsgPack.Unpack<StructMap>(ReadFile("Maps/Struct"));
-			Assert.AreEqual(map.a, 1);
-			Assert.AreEqual(map.b, 2);
-			Assert.AreEqual(map.c, 0);
-			Assert.AreEqual(map.d, null);
+			StructMap map = new StructMap() { a = 0, d = null };
+			byte[] data = MsgPack.Pack<StructMap>(map);
+			StructMap result = MsgPack.Unpack<StructMap>(data);
+			Assert.AreEqual(map.a, result.a);
+			Assert.AreEqual(map.d, result.d);
 		}
 
 		[Test]
 		public void Class()
 		{
-			ClassMap map = MsgPack.Unpack<ClassMap>(ReadFile("Maps/Struct"));
-			Assert.AreEqual(map.a, 1);
-			Assert.AreEqual(map.b, 2);
-			Assert.AreEqual(map.c, 0);
-			Assert.AreEqual(map.d, null);
+			ClassMap map = new ClassMap() { a = 1, b = 2 };
+			byte[] data = MsgPack.Pack<ClassMap>(map);
+			ClassMap result = MsgPack.Unpack<ClassMap>(data);
+			Assert.AreEqual(map.a, result.a);
+			Assert.AreEqual(map.b, result.b);
+			Assert.AreEqual(map.c, result.c);
+			Assert.AreEqual(map.d, result.d);
 		}
 
 		[Test]
 		public void ClassWithPrivateFields()
 		{
-			PrivateClassMap map = MsgPack.Unpack<PrivateClassMap>(ReadFile("Maps/Struct"));
-			Assert.AreEqual(map.A, 1);
-		}
-
-		[Test]
-		public void MapSkippable()
-		{
-			MixedMap map = MsgPack.Unpack<MixedMap>(ReadFile("Maps/MapSkippable"));
-			Assert.AreEqual(map.inner.a, 1);
-			Assert.AreEqual(map.inner.b, 2);
+			PrivateClassMap map = new PrivateClassMap();
+			byte[] data = MsgPack.Pack<PrivateClassMap>(map);
+			PrivateClassMap result = MsgPack.Unpack<PrivateClassMap>(data);
+			Assert.AreEqual(map.A, result.A);
 		}
 
 		[Test]
 		public void FixMapMin()
 		{
-			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/FixMapMin"));
-			Assert.AreEqual(0, dict.Count);
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			byte[] data = MsgPack.Pack<Dictionary<int, int>>(dict);
+			Dictionary<int, int> result = MsgPack.Unpack<Dictionary<int, int>>(data);
+			Assert.AreEqual(Format.FixMapMin, data[0]);
+			Assert.AreEqual(0, result.Count);
 		}
 
 		[Test]
 		public void FixMapMax()
 		{
-			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/FixMapMax"));
-			Assert.AreEqual(15, dict.Count);
-			for(int i = 0; i < 15; i++) {
+			int size = 15;
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			for(int i = 0; i < size; i++) {
+				dict.Add(i, 1);
+			}
+			byte[] data = MsgPack.Pack<Dictionary<int, int>>(dict);
+			Dictionary<int, int> result = MsgPack.Unpack<Dictionary<int, int>>(data);
+			Assert.AreEqual(Format.FixMapMax, data[0]);
+			Assert.AreEqual(size, result.Count);
+			for(int i = 0; i < result.Count; i++) {
 				Assert.AreEqual(1, dict[i]);
 			}
 		}
@@ -88,9 +88,16 @@ namespace UniMsgPack.Tests
 		[Test]
 		public void Map16Min()
 		{
-			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/Map16Min"));
-			Assert.AreEqual(16, dict.Count);
-			for(int i = 0; i < 16; i++) {
+			int size = 16;
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			for(int i = 0; i < size; i++) {
+				dict.Add(i, 1);
+			}
+			byte[] data = MsgPack.Pack<Dictionary<int, int>>(dict);
+			Dictionary<int, int> result = MsgPack.Unpack<Dictionary<int, int>>(data);
+			Assert.AreEqual(Format.Map16, data[0]);
+			Assert.AreEqual(size, result.Count);
+			for(int i = 0; i < result.Count; i++) {
 				Assert.AreEqual(1, dict[i]);
 			}
 		}
@@ -98,9 +105,16 @@ namespace UniMsgPack.Tests
 		[Test]
 		public void Map16Max()
 		{
-			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/Map16Max"));
-			Assert.AreEqual(ushort.MaxValue, dict.Count);
-			for(int i = 0; i < ushort.MaxValue; i++) {
+			int size = ushort.MaxValue;
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			for(int i = 0; i < size; i++) {
+				dict.Add(i, 1);
+			}
+			byte[] data = MsgPack.Pack<Dictionary<int, int>>(dict);
+			Dictionary<int, int> result = MsgPack.Unpack<Dictionary<int, int>>(data);
+			Assert.AreEqual(Format.Map16, data[0]);
+			Assert.AreEqual(size, result.Count);
+			for(int i = 0; i < result.Count; i++) {
 				Assert.AreEqual(1, dict[i]);
 			}
 		}
@@ -108,9 +122,16 @@ namespace UniMsgPack.Tests
 		[Test]
 		public void Map32Min()
 		{
-			Dictionary<int, int> dict = MsgPack.Unpack<Dictionary<int, int>>(ReadFile("Maps/Map32Min"));
-			Assert.AreEqual(ushort.MaxValue + 1, dict.Count);
-			for(int i = 0; i < ushort.MaxValue + 1; i++) {
+			int size = ushort.MaxValue + 1;
+			Dictionary<int, int> dict = new Dictionary<int, int>();
+			for(int i = 0; i < size; i++) {
+				dict.Add(i, 1);
+			}
+			byte[] data = MsgPack.Pack<Dictionary<int, int>>(dict);
+			Dictionary<int, int> result = MsgPack.Unpack<Dictionary<int, int>>(data);
+			Assert.AreEqual(Format.Map16, data[0]);
+			Assert.AreEqual(size, result.Count);
+			for(int i = 0; i < result.Count; i++) {
 				Assert.AreEqual(1, dict[i]);
 			}
 		}
