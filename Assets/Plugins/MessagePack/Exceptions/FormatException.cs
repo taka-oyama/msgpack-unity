0d33f272186d70556094d207d3e3c647e9194fbc
Taka Oyama <taka.oyama@gmail.com>
2018-04-15 00:25:27 +0900

    better exception handling

--- Assets/Plugins/MessagePack/Exceptions/FormatException.cs
+++ Assets/Plugins/MessagePack/Exceptions/FormatException.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using UnityEngine;
 
 namespace MessagePack
 {
@@ -8,7 +10,26 @@ namespace MessagePack
 
 		public FormatException(string message) : base(message) { }
 
-		public FormatException(ITypeHandler handler, Format format, FormatReader reader)
-			: base(handler.GetType() + ": Undefined Format " + format + " in " + " at position " + reader.Position) { }
+		public FormatException(ITypeHandler handler, Format format, FormatReader reader) : base(string.Format("{0}: Undefined Format {1} at position {2}", handler.GetType(), format, reader.stream.Position))
+		{
+			this.Source = RepresetStreamAsJson(reader.stream);
+		}
+
+		public FormatException(Stream stream, Exception inner) : base(string.Format("{0} at Position: {1}", inner.Message, stream.Position), inner)
+		{
+			this.Source = RepresetStreamAsJson(stream);
+		}
+
+		string RepresetStreamAsJson(Stream stream)
+		{
+			string json = null;
+			if(stream.CanSeek) {
+				long prevPosition = stream.Position;
+				stream.Position = 0;
+				json = JsonConverter.Encode(stream);
+				stream.Position = prevPosition;
+			}
+			return json;
+		}
 	}
 }
