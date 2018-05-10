aca246887eb4afacfa63a2dbb924474a748b047b
Takayasu Oyama <t-oyama@colopl.co.jp>
2018-05-10 21:31:23 +0900

    add CamelCaseNamingStrategy

---
+++ Assets/Plugins/SouthPointe/Serialization/MessagePack/Options/NamingStrategy/CamelCaseNamingStrategy.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace SouthPointe.Serialization.MessagePack
+{
+	public class CamelCaseNamingStrategy : IMapNamingStrategy
+	{
+		TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+		public string OnPack(string name, MapDefinition definition)
+		{
+			return char.ToLowerInvariant(name[0]) + name.Substring(1); 
+		}
+
+		public string OnUnpack(string name, MapDefinition definition)
+		{
+			string str = textInfo.ToTitleCase(name).Replace("_", "");
+			return char.ToUpperInvariant(str[0]) + str.Substring(1);
+		}
+	}
+}
