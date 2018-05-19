d2d342c2ab5d089ac61da35b74f5740ad7057808
Taka Oyama <taka.oyama@gmail.com>
2018-05-19 20:41:16 +0900

    make it easier to export package

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class PackageExporter
+{
+	[MenuItem("Custom/Export Package")]
+	static void Export()
+	{
+		string directory = new DirectoryInfo(Application.dataPath + "/..").FullName;
+		string exportPath = "Assets/Plugins/SouthPointe";
+		ExportPackageOptions options = ExportPackageOptions.Recurse | ExportPackageOptions.Default;
+
+		string path = EditorUtility.SaveFilePanel("Export Package", directory, "msgpack-unity", "unitypackage");
+		AssetDatabase.ExportPackage(exportPath, path, options);
+		Debug.LogFormat("Package exported to: {0}", path);
+	}
+}
